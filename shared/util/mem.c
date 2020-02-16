7e2fe43ffce30eb0c6221b45facdb629a47afe89
Stone Tickle <lattis@mochiro.moe>
2020-02-16 12:50:40 +0900

    remove commented test code

--- shared/util/mem.c
+++ shared/util/mem.c
@@ -13,10 +13,6 @@ get_mem(void **elem, size_t size, size_t *len, size_t *cap)
 	if (*len > *cap) {
 		*cap = *cap <= 0 ? DEFAULT_MEM_SIZE : *cap * 2;
 		*elem = realloc(*elem, *cap * size);
-		/*
-		   for (size_t i = 0; i < *cap; i++)
-		        L("  %d => %p", i, *elem + (size * i));
-		 */
 	}
 
 	return *len - 1;
