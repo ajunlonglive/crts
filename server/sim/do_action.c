77e3c48e7a635df855f5833436b77e071160fae5
Stone Tickle <lattis@mochiro.moe>
2020-02-18 15:51:28 +0900

    return action result from do_action calls

--- server/sim/do_action.c
+++ server/sim/do_action.c
@@ -22,29 +22,28 @@ do_action_harvest(struct simulation *sim, struct ent *e, struct sim_action *act)
 		(*harv)++;
 		break;
 	default:
-		if (act->local != NULL && !points_equal(&act->local->goal, &e->pos)) {
-			if (pathfind_and_update(sim, act->local, e) != pr_cont) {
-				L("destroying pgraph");
-				pgraph_destroy(act->local);
-				act->local = NULL;
-			}
+		if (act->local != NULL
+		    && !points_equal(&act->local->goal, &e->pos)
+		    && pathfind_and_update(sim, act->local, e) != pr_cont) {
+			pgraph_destroy(act->local);
+			act->local = NULL;
 		} else if (find_tile(tile_forest, sim->world->chunks, &act->act.range, &np)) {
-			L("creating pgraph, %d, %d", np.x, np.y);
 			act->local = pgraph_create(sim->world->chunks, &np);
 		} else {
 			L("failed to find available tile");
+			return ar_fail;
 		}
 
-		return 0;
+		return ar_cont;
 	}
 
 	if (*harv > 100) {
 		*harv = 0;
 		*cur_tile = tile_plain;
 		queue_push(sim->outbound, sm_create(server_message_chunk, chnk));
-		return 1;
+		return ar_done;
 	} else {
-		return 0;
+		return ar_cont;
 	}
 }
 
@@ -55,6 +54,6 @@ do_action(struct simulation *sim, struct ent *e, struct sim_action *act)
 	case at_harvest:
 		return do_action_harvest(sim, e, act);
 	default:
-		return 1;
+		return ar_done;
 	}
 }
