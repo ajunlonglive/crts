84f63af4293346c467fb9651c41ff11b9148161e
Stone Tickle <lattis@mochiro.moe>
2020-02-18 15:52:06 +0900

    remove limits on action_info

--- shared/constants/action_info.c
+++ shared/constants/action_info.c
@@ -4,6 +4,6 @@
 const struct action_info ACTIONS[] = {
 	/*                  name         maxw   minw   diff.  satis.  */
 	[at_none]       = { "nothing",      0,     0,     0,      0 },
-	[at_move]       = { "move",        50,    50,     1,    100 },
-	[at_harvest]    = { "harvest",     20,    20,    25,    100 },
+	[at_move]       = { "move",      9999,     0,     1,    100 },
+	[at_harvest]    = { "harvest",   9999,     0,    25,    100 },
 };
