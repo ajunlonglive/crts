59ac6532464c02d72664f1ffd8e05d16c7e00ebc
Stone Tickle <lattis@mochiro.moe>
2020-02-19 15:28:19 +0900

    add ent information to global cfg

--- include/shared/constants/globals.h
+++ include/shared/constants/globals.h
@@ -2,15 +2,21 @@
 #define CRTS_GLOBALS_H
 
 #include "shared/sim/action.h"
+#include "shared/sim/ent.h"
 
 struct global_cfg_t {
 	const struct {
 		const char *name;
-		const int max_workers;
-		const int min_workers;
-		const int completed_at;
-		const int satisfaction;
+		const uint16_t max_workers;
+		const uint16_t min_workers;
+		const uint16_t completed_at;
+		const uint16_t satisfaction;
 	} actions[action_type_count];
+
+	const struct {
+		const char *name;
+		const bool animate;
+	} ents[ent_type_count];
 };
 
 extern const struct global_cfg_t gcfg;
--- shared/constants/globals.c
+++ shared/constants/globals.c
@@ -7,5 +7,9 @@ const struct global_cfg_t gcfg = {
 		[at_none]       = { "nothing",      0,     0,     0,      0 },
 		[at_move]       = { "move",      9999,     0,     1,    100 },
 		[at_harvest]    = { "harvest",   9999,     0,    25,    100 },
-	}
+	},
+	.ents = {
+		[et_worker]        = { "worker", true  },
+		[et_resource_wood] = { "wood",   false },
+	},
 };
