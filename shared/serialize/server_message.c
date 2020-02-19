e60d7363b698c6f2256436e17366ecd52f572594
Stone Tickle <lattis@mochiro.moe>
2020-02-19 11:00:32 +0900

    manual serialization over memcpy for server struct
    Since server structs are going to be bigger than client structs, it
    doesnt really make sense to serialize and deserialize them by just
    memcpying, since the sizeof the server one is > than the sizeof the
    client.  I think this will still work if all of the server specific
    fields are at the end of the struct, but it will definitely result in
    wasted bandwith (copying stuff we don't care about with memcpy, and
    transfering it over the network, just for it to be ignored).

--- shared/serialize/server_message.c
+++ shared/serialize/server_message.c
@@ -32,29 +32,61 @@ pack_sm_ent(const struct sm_ent *eu, char *buf)
 size_t
 pack_sm_chunk(const struct sm_chunk *eu, char *buf)
 {
-	memcpy(buf, &eu->chunk, sizeof(struct chunk));
-	return sizeof(struct chunk);
+	size_t b = 0, tiles = sizeof(enum tile) * CHUNK_SIZE * CHUNK_SIZE;
+
+	b += pack_point(&eu->chunk.pos, &buf[b]);
+
+	memcpy(&buf[b], &eu->chunk.tiles, tiles);
+	b += tiles;
+
+	b += pack_bool(&eu->chunk.empty, &buf[b]);
+
+	return b;
 };
 
 size_t
 unpack_sm_chunk(struct sm_chunk *eu, const char *buf)
 {
-	memcpy(&eu->chunk, buf, sizeof(struct chunk));
-	return sizeof(struct chunk);
+	size_t b = 0, tiles = sizeof(enum tile) * CHUNK_SIZE * CHUNK_SIZE;
+
+	b += unpack_point(&eu->chunk.pos, &buf[b]);
+
+	memcpy(&eu->chunk.tiles, &buf[b], tiles);
+	b += tiles;
+
+	b += unpack_bool(&eu->chunk.empty, &buf[b]);
+
+	return b;
 };
 
 size_t
 pack_sm_action(const struct sm_action *eu, char *buf)
 {
-	memcpy(buf, &eu->action, sizeof(struct action));
-	return sizeof(struct action);
+	size_t b = 0;
+
+	memcpy(&buf[b], &eu->action.type, sizeof(enum action_type));
+	b += sizeof(enum action_type);
+
+	b += pack_circle(&eu->action.range, &buf[b]);
+	b += pack_uint8_t(&eu->action.workers_requested, &buf[b]);
+	b += pack_uint8_t(&eu->action.id, &buf[b]);
+
+	return b;
 };
 
 size_t
 unpack_sm_action(struct sm_action *eu, const char *buf)
 {
-	memcpy(&eu->action, buf, sizeof(struct action));
-	return sizeof(struct action);
+	size_t b = 0;
+
+	memcpy(&eu->action.type, &buf[b], sizeof(enum action_type));
+	b += sizeof(enum action_type);
+
+	b += unpack_circle(&eu->action.range, &buf[b]);
+	b += unpack_uint8_t(&eu->action.workers_requested, &buf[b]);
+	b += unpack_uint8_t(&eu->action.id, &buf[b]);
+
+	return b;
 };
 
 size_t
