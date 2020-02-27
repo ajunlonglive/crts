8936f1d8ec0ecfd9b15c1ec00d45202eb79ce779
Stone Tickle <lattis@mochiro.moe>
2020-02-28 08:58:09 +0900

    write more detailed info to display

--- client/display/info.c
+++ client/display/info.c
@@ -1,3 +1,5 @@
+#include <curses.h>
+
 #include "client/display/info.h"
 #include "client/display/window.h"
 #include "client/hiface.h"
@@ -8,14 +10,18 @@ draw_infol(struct win *win, struct hiface *hif)
 {
 	struct point p = { 0, 0 };
 
-	win_printf(win, &p, "simlation running (%c)", hif->redrew_world ? '!' : '_');
+	win_printf(win, &p, "redraws: (+%6d) | ping: (+%6uf)", hif->redrew_world,
+		hif->server_timeout);
+	clrtoeol();
 	p.y++;
 	win_printf(win, &p, "view: (%d, %d) | cursor: (%d, %d)",
 		hif->view.x, hif->view.y,
 		hif->cursor.x + hif->view.x,
 		hif->cursor.y + hif->view.y);
+	clrtoeol();
 	p.y++;
 	win_printf(win, &p, "cmd: %s%s", hif->num.buf, hif->cmd.buf);
+	clrtoeol();
 }
 
 void
@@ -23,5 +29,6 @@ draw_infor(struct win *win, struct world *w)
 {
 	struct point p = { 0, 0 };
 
-	win_printf(win, &p, "total entities: %d", w->ents.len);
+	win_printf(win, &p, "ents : % ld, chunks:% ld ", w->ents.len, hdarr_len(w->chunks->hd));
+	clrtoeol();
 }
