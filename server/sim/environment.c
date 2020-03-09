3c570f7285c5f45c660325524dba1ddc8a4cc9eb
Stone Tickle <lattis@mochiro.moe>
2020-03-09 14:15:00 +0900

    server:sim:environment: use update_tile_at

--- server/sim/environment.c
+++ server/sim/environment.c
@@ -6,16 +6,16 @@
 
 #include "server/sim/environment.h"
 #include "server/sim/sim.h"
+#include "server/sim/terrain.h"
 #include "shared/constants/globals.h"
 
 #define GROW_CHANCE 100 // 1 in
 
-static bool
-process_chunk(struct chunk *ck)
+static void
+process_chunk(struct chunks *cnks, struct chunk *ck)
 {
 	size_t x, y;
 	enum tile t;
-	bool changed = false;
 
 	for (x = 0; x < CHUNK_SIZE; ++x) {
 		for (y = 0; y < CHUNK_SIZE; ++y) {
@@ -25,12 +25,9 @@ process_chunk(struct chunk *ck)
 
 			t = ck->tiles[x][y];
 
-			ck->tiles[x][y] = gcfg.tile_lifecycle[t].next;
-			changed = true;
+			update_tile_at(cnks, ck, x, y, t);
 		}
 	}
-
-	return changed;
 }
 
 void
@@ -46,7 +43,5 @@ process_environment(struct simulation *sim)
 
 	struct chunk *ck = hdarr_get_by_i(sim->world->chunks->hd, ri);
 
-	if (process_chunk(ck)) {
-		ck->last_touched = ++sim->world->chunks->chunk_date;
-	}
+	process_chunk(sim->world->chunks, ck);
 }
