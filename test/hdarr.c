d30c923308f622413d5b5dcfb89b9e13d3fee105
Stone Tickle <lattis@mochiro.moe>
2020-03-10 15:58:39 +0900

    test: add a stress test for hdarr

---
+++ test/hdarr.c
@@ -0,0 +1,60 @@
+#define _XOPEN_SOURCE 500
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "shared/types/hdarr.h"
+#include "shared/util/log.h"
+
+#define LOOPS 0xfffff
+#define INI_SIZE 2
+//#define DLEN 87
+#define DLEN 1024
+#define SEED 1235
+
+typedef uint32_t intt;
+
+static void *
+hkgetter(void *e)
+{
+	return e;
+}
+
+int
+main(int argc, const char * const *argv)
+{
+	struct hdarr *hd = hdarr_init(INI_SIZE, sizeof(intt), sizeof(intt), hkgetter);
+	size_t i;
+	intt k;
+
+	srandom(SEED);
+
+	for (i = 0; i < DLEN; ++i) {
+		hdarr_set(hd, &i, &i);
+	}
+
+	intt *vp;
+	for (i = 0; i < LOOPS; ++i) {
+		k = random() % DLEN;
+
+		if (random() % 2 == 0) {
+			//L("checking %d", k);
+			if ((vp = hdarr_get(hd, &k)) != NULL) {
+				assert(k == *vp);
+			} else {
+				//L("setting %dd to %dz", k, k);
+				hdarr_set(hd, &k, &k);
+				assert(hdarr_get(hd, &k) != NULL);
+			}
+		} else {
+			//L("deleting %d", k);
+			hdarr_del(hd, &k);
+			assert(hdarr_get(hd, &k) == NULL);
+		}
+	}
+
+	hdarr_destroy(hd);
+
+	return 0;
+}
