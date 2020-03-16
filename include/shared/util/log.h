d76f0cafdf3f3d44d4c728b13510f8217a51a0f1
Stone Tickle <lattis@mochiro.moe>
2020-03-16 11:37:54 +0900

    log: add valgrind backtrace assert and meson opt

--- include/shared/util/log.h
+++ include/shared/util/log.h
@@ -1,10 +1,15 @@
-#ifndef __LOG_H
-#define __LOG_H
+#ifndef SHARED_UTIL_LOG_H
+#define SHARED_UTIL_LOG_H
 
+#ifdef NDEBUG
+#define L(...)
+#define LB(src, size)
+#define VBASSERT(exp, ...)
+#else
+#include <assert.h>
 #include <stdio.h>
 
 #define L(...) do { \
-		log_timestamp(); \
 		fprintf(stderr, "%s:%d [\e[35m%s\e[0m] ", __FILE__, __LINE__, __func__); \
 		fprintf(stderr, __VA_ARGS__); \
 		fprintf(stderr, "\n"); \
@@ -16,6 +21,28 @@
 		fprintf(stderr, "\n"); \
 } while (0)
 
+
+#ifdef VALGRIND_BACKTRACE
+#include <valgrind/valgrind.h>
+#define VBASSERT(exp, ...) do { \
+		if (!(exp)) { \
+			if (RUNNING_ON_VALGRIND) { \
+				VALGRIND_PRINTF_BACKTRACE(__VA_ARGS__); \
+			} else { \
+				L(__VA_ARGS__); \
+			} \
+		} \
+		assert(exp); \
+} while (0)
+#else
+#define VBASSERT(exp, ...) do { \
+		if (!(exp)) { \
+			L(__VA_ARGS__); \
+		} \
+		assert(exp); \
+} while (0)
+#endif
+#endif
+
 void log_bytes(const void *src, size_t size);
-void log_timestamp(void);
 #endif
--- shared/util/log.c
+++ shared/util/log.c
@@ -14,14 +14,3 @@ log_bytes(const void *src, size_t size)
 		fprintf(stderr, "%02hhx", bytes[i]);
 	}
 }
-
-void
-log_timestamp(void)
-{
-	struct timespec ts;
-	static long start_time = 1584157636;
-
-	clock_gettime(CLOCK_REALTIME, &ts);
-
-	fprintf(stderr, "%ld.%05ld", ts.tv_sec - start_time, ts.tv_nsec / 10000);
-}
