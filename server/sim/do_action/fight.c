fd7b1607a183ad1ffe93c2e907a62b788f4f3b4c
Stone Tickle <lattis@mochiro.moe>
2020-03-25 17:04:38 +0900

    do_action:fight: fight targets all animate ents

--- server/sim/do_action/fight.c
+++ server/sim/do_action/fight.c
@@ -6,6 +6,7 @@
 #include "server/sim/do_action.h"
 #include "server/sim/do_action/fight.h"
 #include "server/sim/terrain.h"
+#include "shared/constants/globals.h"
 #include "shared/sim/ent.h"
 #include "shared/types/result.h"
 #include "shared/util/log.h"
@@ -20,7 +21,7 @@ find_enemy_pred(void *_ctx, struct ent *e)
 {
 	struct find_enemey_pred_ctx *ctx = _ctx;
 
-	return point_in_circle(&e->pos, ctx->range) && e->type == et_worker
+	return point_in_circle(&e->pos, ctx->range) && gcfg.ents[e->type].animate
 	       && e->alignment != ctx->e->alignment;
 }
 
