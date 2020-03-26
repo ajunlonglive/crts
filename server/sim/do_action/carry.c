ba25b12aa1028ca453f3120f2d892c2d413e2176
Stone Tickle <lattis@mochiro.moe>
2020-03-26 14:59:30 +0900

    do_action_carry: use source as dropoff point

--- server/sim/do_action/carry.c
+++ server/sim/do_action/carry.c
@@ -43,7 +43,7 @@ do_action_carry(struct simulation *sim, struct ent *e, struct sim_action *sa)
 	}
 
 	if (e->holding) {
-		switch (dropoff_resources(sim, e, &sa->act.range.center)) {
+		switch (dropoff_resources(sim, e, &sa->act.source.center)) {
 		case rs_cont:
 			break;
 		case rs_done:
