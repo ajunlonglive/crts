f7629424dab866c3119c940c07136595c553b97f
Stone Tickle <lattis@mochiro.moe>
2020-04-30 06:24:15 +0900

    GLFW: set forward compat on macos

--- client/ui/opengl/winutil.c
+++ client/ui/opengl/winutil.c
@@ -66,6 +66,9 @@ init_window(void)
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
