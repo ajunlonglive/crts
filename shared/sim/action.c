63e7b44735ae62e3e2291158b01d7a814f1f47b6
Stone Tickle <lattis@mochiro.moe>
2020-05-08 20:44:30 +0900

    avoid # inside macro expansion

--- shared/sim/action.c
+++ shared/sim/action.c
@@ -14,22 +14,29 @@ action_init(struct action *act)
 void
 action_inspect(struct action *act)
 {
+#ifdef CRTS_SERVER
 	L(
 		"action type: %d\n"
-#ifdef CRTS_SERVER
 		"  id: %u owner: %u\n"
 		"  workers (r: %u, a: %u)\n"
-#endif
 		"  range: (%d, %d) r: %d",
 		act->type,
-#ifdef CRTS_SERVER
 		act->id,
 		act->motivator,
 		act->workers_requested,
 		act->workers_assigned,
-#endif
 		act->range.center.x,
 		act->range.center.y,
 		act->range.r
 		);
+#else
+	L(
+		"action type: %d\n"
+		"  range: (%d, %d) r: %d",
+		act->type,
+		act->range.center.x,
+		act->range.center.y,
+		act->range.r
+		);
+#endif
 }
