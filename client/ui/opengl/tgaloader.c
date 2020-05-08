9b2ed56bfaf3fd0e4bd8fa56483d035f57a48b04
Stone Tickle <lattis@mochiro.moe>
2020-05-08 20:15:38 +0900

    write a simple tga loader

---
+++ client/ui/opengl/tgaloader.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "client/ui/opengl/tgaloader.h"
+#include "shared/util/log.h"
+
+#define CHUNK_SIZE 512
+
+uint8_t *
+load_tga(const char *path)
+{
+	FILE *f;
+	size_t len, b, i = 0;
+
+	if (!(f = fopen(path, "r"))) {
+		L("error opening font atlas '%s': %s", path, strerror(errno));
+		return NULL;
+	}
+
+	fseek(f, 0, SEEK_END);
+	len = ftell(f);
+	rewind(f);
+
+	fseek(f, 18, SEEK_SET);
+
+	uint8_t *buf = malloc(sizeof(char) * (len - 18));
+
+	while ((b = fread(&buf[i], 1, CHUNK_SIZE, f))) {
+		if (i == 0) {
+			L("%d, %d, %d, %d", buf[0], buf[1], buf[2], buf[3]);
+		}
+		i += b;
+	}
+
+	L("read %ld bytes into buffer(%ld)", i, len);
+
+	return buf;
+}
---
+++ include/client/ui/opengl/tgaloader.h
@@ -0,0 +1,6 @@
+#ifndef CLIENT_UI_OPENGL_TGALOADER_H
+#define CLIENT_UI_OPENGL_TGALOADER_H
+#include <stdint.h>
+
+uint8_t *load_tga(const char *path);
+#endif
