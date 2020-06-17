b71bda1035cd983d3e8972c72c093af24ece38db
Stone Tickle <lattis@mochiro.moe>
2020-06-17 13:57:52 +0900

    add tests for inih

---
+++ test/inih.c
@@ -0,0 +1,67 @@
+#include "posix.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "shared/util/log.h"
+#include "shared/util/inih.h"
+
+char *test_ini_file =
+	"; comment\n"         //  1
+	"\n"                  //  2
+	"[ect_1]\n"          //  3
+	"a = 1\n"             //  4
+	"b   =    2 \n"       //  5
+	"\n"                  //  6
+	"; another comment\n" //  7
+	"[sect_2]\n"          //  8
+	"a = 3\n"             //  9
+;
+
+#define check_line(s1, k1, v1) assert(strcmp(s1, sect) == 0 && strcmp(k1, k) == 0 && strcmp(v1, v) == 0)
+
+static bool
+ini_parse_cb(void *_ctx, const char *sect, const char *k, const char *v, uint32_t line)
+{
+	size_t *found = _ctx;
+
+	switch (line) {
+	case 4:
+		check_line("sect_1", "a", "1");
+		break;
+	case 5:
+		check_line("sect_1", "b", "2 ");
+		break;
+	case 9:
+		check_line("sect_2", "a", "3");
+		break;
+	default:
+		assert(false);
+		break;
+	}
+
+	++(*found);
+
+	return true;
+}
+
+int
+main(int argc, char *const *argv)
+{
+	size_t found = 0;
+	struct file_data test_ini = { .path = "" };
+
+	test_ini.len = strlen(test_ini_file);
+	test_ini.data = malloc(test_ini.len);
+	memcpy(test_ini.data, test_ini_file, test_ini.len);
+
+	log_level = ll_debug;
+
+	bool res = ini_parse(&test_ini, ini_parse_cb, &found);
+	assert(found == 3);
+
+	free(test_ini.data);
+
+	return !res; /* false = 0 */
+}
