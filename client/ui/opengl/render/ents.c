312f4397e85fdac93e1f2579dd69d5b3ae0d741d
Stone Tickle <lattis@mochiro.moe>
2020-06-30 11:18:52 +0900

    update ent renderer with new shader layer

--- client/ui/opengl/render/ents.c
+++ client/ui/opengl/render/ents.c
@@ -5,146 +5,120 @@
 #include "client/ui/opengl/loaders/obj.h"
 #include "client/ui/opengl/loaders/shader.h"
 #include "client/ui/opengl/render/ents.h"
+#include "client/ui/opengl/shader.h"
 #include "shared/util/log.h"
 
-static struct {
-	uint32_t id;
-	uint32_t vao, vbo, ebo;
-	uint32_t view, proj, view_pos, positions, types, colors;
-	size_t triangle_count;
-} s_ent = { 0 };
+typedef float ent_info[7];
+struct shader ent_shader = { 0 };
+struct darr *entity_data;
+size_t indices_len;
 
 bool
 render_world_setup_ents(void)
 {
-	bool init = false;
-
-	struct shader_src src[] = {
-		{ "ents.vert", GL_VERTEX_SHADER },
-		{ "world.frag", GL_FRAGMENT_SHADER },
-		{ "\0" }
-	};
-
-	if (!link_shaders(src, &s_ent.id)) {
-		return false;
-	}
-
-	glUseProgram(s_ent.id);
-
-	s_ent.view      = glGetUniformLocation(s_ent.id, "view");
-	s_ent.proj      = glGetUniformLocation(s_ent.id, "proj");
-	s_ent.positions = glGetUniformLocation(s_ent.id, "positions");
-	s_ent.types     = glGetUniformLocation(s_ent.id, "types");
-	s_ent.colors    = glGetUniformLocation(s_ent.id, "colors");
-	s_ent.view_pos  = glGetUniformLocation(s_ent.id, "view_pos");
-
-	glGenVertexArrays(1, &s_ent.vao);
-	glGenBuffers(1, &s_ent.vbo);
-	glGenBuffers(1, &s_ent.ebo);
-
-	struct darr *obj_verts   = darr_init(sizeof(vertex_elem));
+	struct darr *obj_verts = darr_init(sizeof(vertex_elem));
 	struct darr *obj_indices = darr_init(sizeof(uint32_t));
+	entity_data = darr_init(sizeof(ent_info));
+
 	if (!obj_load("cube.obj", obj_verts, obj_indices, 1.0f)) {
 		LOG_W("failed to load asset");
 		goto free_exit;
 	}
 
-	s_ent.triangle_count = darr_len(obj_indices);
+	indices_len = darr_len(obj_indices);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, s_ent.ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * darr_len(obj_indices),
-		darr_raw_memory(obj_indices), GL_STATIC_DRAW);
+	struct shader_spec ent_spec = {
+		.src = {
+			{ "ents.vert", GL_VERTEX_SHADER },
+			{ "world.frag", GL_FRAGMENT_SHADER },
+		},
+		.attribute = {
+			{ 3, GL_FLOAT }, { 3, GL_FLOAT },
+			{ 3, GL_FLOAT, true }, { 3, GL_FLOAT, true }, { 1, GL_FLOAT, true }
+		},
+		.object = {
+			.indices_len = sizeof(uint32_t) * darr_len(obj_indices),
+			.indices = darr_raw_memory(obj_indices),
+			.verts_len = sizeof(vertex_elem) * darr_len(obj_verts),
+			.verts = darr_raw_memory(obj_verts),
+		},
+	};
 
-	glBindBuffer(GL_ARRAY_BUFFER, s_ent.vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_elem) * darr_len(obj_verts),
-		darr_raw_memory(obj_verts), GL_STATIC_DRAW);
+	if (!shader_create(&ent_spec, &ent_shader)) {
+		goto free_exit;
+	}
 
-	glBindVertexArray(s_ent.vao);
+	darr_destroy(obj_verts);
+	darr_destroy(obj_indices);
 
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_elem),
-		(void *)0);
-	glEnableVertexAttribArray(0);
-
-	// normal attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex_elem),
-		(void *)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	// send colors
-	glUniform4fv(s_ent.colors, extended_ent_type_count, (float *)colors.ent);
-
-	init = true;
-
+	return true;
 free_exit:
 	darr_destroy(obj_verts);
 	darr_destroy(obj_indices);
 
-	return init;
+	return false;
 }
 
 void
 render_ents(struct hiface *hf, struct opengl_ui_ctx *ctx)
 {
 	struct ent *emem = darr_raw_memory(hdarr_darr(hf->sim->w->ents));
-	size_t i, j = 0, len = hdarr_len(hf->sim->w->ents);
-
-	float positions[256 * 3] = { 0 };
-	uint32_t types[256] = { 0 };
-	size_t skipped = 0, rendered = 0;
-
-	glUseProgram(s_ent.id);
-	glBindVertexArray(s_ent.vao);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, s_ent.ebo);
-	glBindBuffer(GL_ARRAY_BUFFER, s_ent.vbo);
-
-	if (cam.changed) {
-		glUniformMatrix4fv(s_ent.proj, 1, GL_TRUE, (float *)ctx->mproj);
-		glUniformMatrix4fv(s_ent.view, 1, GL_TRUE, (float *)ctx->mview);
-		glUniform3fv(s_ent.view_pos, 1, cam.pos);
-	}
+	size_t i, len = hdarr_len(hf->sim->w->ents);
 
 	for (i = 0; i < len; ++i) {
 		if (!point_in_rect(&emem[i].pos, &ctx->ref)) {
-			++skipped;
 			continue;
 		}
 
 		struct point p = nearest_chunk(&emem[i].pos);
 		struct chunk *ck = hdarr_get(hf->sim->w->chunks->hd, &p);
 
-		positions[(j * 3) + 0] = emem[i].pos.x;
-		positions[(j * 3) + 1] = emem[i].pos.y;
+		float height = 0.0;
+		uint32_t type = emem[i].type;
+
 		if (ck) {
 			p = point_sub(&emem[i].pos, &ck->pos);
 			if (ck->tiles[p.x][p.y] <= tile_water) {
-				positions[(j * 3) + 2] = -2.0;
+				height = -2.0;
 			} else {
-				positions[(j * 3) + 2] = 0.5 + ck->heights[p.x][p.y];
+				height = 0.5 + ck->heights[p.x][p.y];
 			}
 		}
 
-		if ((types[j] = emem[i].type) == et_worker) {
+		if (type == et_worker) {
 			if (emem[i].alignment == hf->sim->assigned_motivator) {
-				types[j] = et_elf_friend;
+				type = et_elf_friend;
 			} else {
-				types[j] = et_elf_foe;
+				type = et_elf_foe;
 			}
 		}
 
-		if (++j >= 256) {
-			glUniform1uiv(s_ent.types, j, types);
-			glUniform3fv(s_ent.positions, j, positions);
-			//glDrawArraysInstanced(GL_POINTS, 0, 540, j);
-			glDrawElementsInstanced(GL_TRIANGLES, s_ent.triangle_count, GL_UNSIGNED_INT, 0, j);
-			j = 0;
-		}
+		ent_info info = {
+			emem[i].pos.x,
+			height,
+			emem[i].pos.y,
+			colors.ent[type][0],
+			colors.ent[type][1],
+			colors.ent[type][2],
+			0.5
+		};
 
-		++rendered;
+		darr_push(entity_data, info);
 	}
 
-	glUniform1uiv(s_ent.types, j, types);
-	glUniform3fv(s_ent.positions, j, positions);
-	//glDrawArraysInstanced(GL_POINTS, 0, 540, j);
-	glDrawElementsInstanced(GL_TRIANGLES, s_ent.triangle_count, GL_UNSIGNED_INT, 0, j);
+	glBindBuffer(GL_ARRAY_BUFFER, ent_shader.buffer[bt_ivbo]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ent_info) * darr_len(entity_data),
+		darr_raw_memory(entity_data), GL_DYNAMIC_DRAW);
+
+	shader_use(&ent_shader);
+	shader_check_cam(&ent_shader, ctx);
+
+	glDrawElementsInstanced(
+		GL_TRIANGLES,
+		indices_len,
+		GL_UNSIGNED_INT,
+		(void *)0,
+		darr_len(entity_data));
+
+	darr_clear(entity_data);
 }
--- client/ui/opengl/shaders/ents.vert
+++ client/ui/opengl/shaders/ents.vert
@@ -3,32 +3,32 @@
 layout (location = 0) in vec3 vertex;
 layout (location = 1) in vec3 in_normal;
 
+layout (location = 2) in vec3 pos_offset;
+layout (location = 3) in vec3 color;
+layout (location = 4) in float scale;
+
 out vec3 frag_pos;
 flat out vec3 normal;
 flat out vec4 inclr;
 
 uniform mat4 view;
 uniform mat4 proj;
-
-uniform vec3 positions[256];
-uniform uint types[256];
-
 uniform vec4 colors[256];
 
 void
 main()
 {
-	uint type = types[gl_InstanceID];
-
 	mat4 model = mat4(
-		0.5, 0, 0, 0,
-		0, 0.5, 0, 0,
-		0, 0, 0.5, 0,
-		positions[gl_InstanceID].xzy, 1
+		scale, 0, 0, 0,
+		0, scale, 0, 0,
+		0, 0, scale, 0,
+		pos_offset.xyz, 1
 	);
 
-	frag_pos = vec3(model * vec4(vertex, 1.0));
-	gl_Position = proj * view * model * vec4(vertex, 1.0);
+	vec4 pos = model * vec4(vertex, 1.0);
+
+	frag_pos = pos.xyz;
+	gl_Position = proj * view * pos;
 	normal = in_normal;
-	inclr = vec4(colors[type].xyz, 1.0);
+	inclr = vec4(color, 1.0);
 }
