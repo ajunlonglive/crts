efe65e09944d9f5e6880fd262abde74fd7caf568
Stone Tickle <lattis@mochiro.moe>
2020-09-25 19:20:28 +0900

    expermient: SOA vs AOS compression
    the result is no difference at all, which is what I expected

--- test/shrink.c
+++ test/shrink.c
@@ -70,6 +70,56 @@ test_uint16_t(uint16_t i)
 	return v == i;
 }
 
+static void
+pack_first_part(struct ac_coder *c)
+{
+	c->lim = UINT16_MAX;
+	ac_pack(c, 1);
+}
+
+static void
+pack_second_part(struct ac_coder *c)
+{
+	c->lim = 4;
+	ac_pack(c, 2);
+}
+
+#define ALEN 512
+#define TPMBUF (16 * ALEN)
+static void
+test_packing_methods(void)
+{
+	uint32_t i;
+	struct ac_coder c1 = { 0 }, c2 = { 0 };
+	uint8_t buf1[TPMBUF] = { 0 }, buf2[TPMBUF] = { 0 };
+	ac_pack_init(&c1, buf1, TPMBUF);
+	ac_pack_init(&c2, buf2, TPMBUF);
+
+	/* method 1: array of structs */
+	for (i = 0; i < ALEN; ++i) {
+		pack_first_part(&c1);
+		pack_second_part(&c1);
+	}
+
+	ac_pack_finish(&c1);
+
+	/* method 2: struct of arrays */
+	for (i = 0; i < ALEN; ++i) {
+		pack_first_part(&c2);
+	}
+
+	for (i = 0; i < ALEN; ++i) {
+		pack_second_part(&c2);
+	}
+
+	ac_pack_finish(&c2);
+
+	L("AOS: %ld (%d) | SOA: %ld (%d) | NONE: %d",
+		ac_coder_len(&c1), c1.bufi,
+		ac_coder_len(&c2), c2.bufi,
+		ALEN * 8);
+}
+
 int32_t
 main(int32_t argc, const char *const argv[])
 {
@@ -90,6 +140,8 @@ main(int32_t argc, const char *const argv[])
 	for (uint32_t i = 0; i < UINT16_MAX; ++i) {
 		assert(test_uint16_t(i));
 	}
+
+	test_packing_methods();
 
 	return 0;
 }
