53bfff1d892dcb96193016e9e3dc32762326d1ce
Stone Tickle <lattis@mochiro.moe>
2020-11-17 11:50:46 +0900

    make hdarr test more brutal

--- test/hdarr.c
+++ test/hdarr.c
@@ -7,11 +7,6 @@
 #include "shared/types/hdarr.h"
 #include "shared/util/log.h"
 
-#define DLEN 0xffff
-#define INI_SIZE 2
-#define LOOPS 0xfffff
-#define SEED 1235
-
 typedef uint32_t intt;
 
 static const void *
@@ -20,48 +15,71 @@ hkgetter(void *e)
 	return e;
 }
 
-int
-main(int argc, const char * const *argv)
+static void
+stress_test(uint32_t ini_size, uint32_t loops, uint32_t dlen, uint32_t ini_set)
 {
-	struct hdarr *hd = hdarr_init(INI_SIZE, sizeof(intt), sizeof(intt), hkgetter);
+	struct hdarr *hd = hdarr_init(ini_size, sizeof(intt), sizeof(intt), hkgetter);
 	size_t i;
 	intt k;
 
-	srand(SEED);
-
-	for (i = 0; i < 12345; ++i) {
-		k = rand() % DLEN;
+	for (i = 0; i < ini_set; ++i) {
+		k = rand() % dlen;
 
 		hdarr_set(hd, &k, &k);
 	}
 
 	intt *vp;
-	for (i = 0; i < LOOPS; ++i) {
-		if (i % 0xffff == 0) {
-			L("%lx / %x", i, LOOPS);
-		}
-
-		k = rand() % DLEN;
+	for (i = 0; i < loops; ++i) {
+		k = rand() % dlen;
 
 		if (rand() % 4 == 0) {
-			if ((vp = hdarr_get(hd, &k)) != NULL) {
-				if (k != *vp) {
-					L("failing");
-				}
+			if ((vp = hdarr_get(hd, &k))) {
 				assert(k == *vp);
 			} else {
 				hdarr_set(hd, &k, &k);
-
-				assert(hdarr_get(hd, &k) != NULL);
+				assert(hdarr_get(hd, &k));
 			}
 		} else {
 			hdarr_del(hd, &k);
-
-			assert(hdarr_get(hd, &k) == NULL);
+			assert(!hdarr_get(hd, &k));
 		}
 	}
 
 	hdarr_destroy(hd);
+}
+
+uint32_t
+pow2(uint32_t n)
+{
+	uint32_t i, v = 1;
+
+	for (i = 1; i < n; ++i) {
+		v *= 2;
+	}
+
+	return v;
+}
+
+int
+main(int argc, const char * const *argv)
+{
+	log_init();
+	log_level = ll_info;
+
+	stress_test(16, 16, 16, 16);
+
+	srand(12346);
+
+	uint32_t i, a, b, c, d;
+	for (i = 0; i < 1000; ++i) {
+		a = pow2((rand() % 14) + 4);
+		b = rand() % 100000;
+		c = rand() % 100000;
+		d = rand() % 100000;
+
+		LOG_I("stress_test(%d, %d, %d, %d)", a, b, c, d);
+		stress_test(a, b, c, d);
+	}
 
 	return 0;
 }
