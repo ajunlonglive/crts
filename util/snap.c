62ef5f8e8e01136dd34590b8c0617031eed5f0e0
Stone Tickle <lattis@mochiro.moe>
2020-12-23 09:39:26 +0900

    snap: calulate image dimensions from world size

--- util/snap.c
+++ util/snap.c
@@ -23,20 +23,45 @@ main(int32_t argc, char * const *argv)
 	}
 
 	struct chunks chunks;
+	struct chunk *ck;
 	chunks_init(&chunks);
 
 	if (!load_world_from_path(argv[1], &chunks)) {
 		return 1;
 	}
 
-	uint32_t height = 256, width = 256;
+	uint32_t i;
+	int32_t maxx = INT32_MIN, maxy = INT32_MIN, minx = INT32_MAX, miny = INT32_MAX;
+
+	for (i = 0; i < hdarr_len(&chunks.hd); ++i) {
+		ck = hdarr_get_by_i(&chunks.hd, i);
+
+		if (ck->pos.x > maxx) {
+			maxx = ck->pos.x;
+		}
+
+		if (ck->pos.x < minx) {
+			minx = ck->pos.x;
+		}
+
+		if (ck->pos.y > maxy) {
+			maxy = ck->pos.y;
+		}
+
+		if (ck->pos.y < miny) {
+			miny = ck->pos.y;
+		}
+	}
+
+	uint32_t height = maxy - miny, width = maxx - minx;
+
 	write_tga_hdr(stdout, height, width);
 
 	struct point p = { 0, 0 };
 	struct chunk *c = get_chunk(&chunks, &p);
 
-	for (p.y = 0; p.y < (int32_t)height; ++p.y) {
-		for (p.x = 0; p.x < (int32_t)width; ++p.x) {
+	for (p.y = miny; p.y < maxy; ++p.y) {
+		for (p.x = minx; p.x < maxx; ++p.x) {
 			if (nearest_chunk(&p).x != c->pos.x) {
 				c = get_chunk_at(&chunks, &p);
 			}
