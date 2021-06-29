121d17a2b98168e3f70e35ac494a829e900c2f5c
Stone Tickle <lattis@mochiro.moe>
2021-06-29 14:09:49 +0900

    implement a basic ring buffer

---
+++ include/shared/types/ring_buffer.h
@@ -0,0 +1,17 @@
+#ifndef SHARED_TYPES_RING_BUFFER_H
+#define SHARED_TYPES_RING_BUFFER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+struct ring_buffer {
+	uint8_t *buf;
+	uint32_t item_size, mask;
+	_Atomic uint32_t read, write;
+};
+
+
+void ring_buffer_init(struct ring_buffer *rb, uint32_t item_size, uint32_t len);
+void *ring_buffer_pop(struct ring_buffer *rb);
+bool ring_buffer_push(struct ring_buffer *rb, void *val);
+#endif
---
+++ shared/types/ring_buffer.c
@@ -0,0 +1,50 @@
+#include "posix.h"
+
+#include <assert.h>
+#include <string.h>
+
+#include "shared/types/ring_buffer.h"
+#include "shared/util/mem.h"
+
+/* inspired by:
+ * https://www.snellman.net/blog/archive/2016-12-13-ring-buffers/
+ */
+
+void
+ring_buffer_init(struct ring_buffer *rb, uint32_t item_size, uint32_t len)
+{
+	*rb = (struct ring_buffer) {
+		.item_size = item_size,
+		.mask = len - 1,
+		.buf = z_malloc(item_size * len),
+	};
+
+	assert((len & rb->mask) == 0 && "ring buffer size must be a power of 2");
+}
+
+void *
+ring_buffer_pop(struct ring_buffer *rb)
+{
+	void *ret = NULL;
+
+	if (rb->read != rb->write) {
+		ret = &rb->buf[(rb->read & rb->mask) * rb->item_size];
+		++rb->read;
+	}
+
+	return ret;
+}
+
+bool
+ring_buffer_push(struct ring_buffer *rb, void *val)
+{
+	bool ret = false;
+
+	if (rb->write - rb->read != (rb->mask + 1)) {
+		memcpy(&rb->buf[(rb->write & rb->mask) * rb->item_size], val, rb->item_size);
+		++rb->write;
+		ret = true;
+	}
+
+	return ret;
+}
