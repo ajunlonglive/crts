b28aa8a18bd3f7261bd9a282b82f0ba6236ad265
Stone Tickle <lattis@mochiro.moe>
2021-07-13 10:48:03 +0900

    restructure launcher to allow multiple games/sesh

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -62,6 +62,11 @@ main(int argc, char *const argv[])
 	log_init();
 	launcher_assets_init();
 
+	static struct server server = { 0 };
+	static struct client client = { 0 };
+	static struct sock_addr server_addr = { 0 };
+	static struct msgr_transport_rudp_ctx server_rudp_ctx = { 0 }, client_rudp_ctx = { 0 };
+
 	static bool always_true = true;
 	struct runtime rt = { .run = &always_true };
 	struct opts opts = { 0 };
@@ -70,89 +75,86 @@ main(int argc, char *const argv[])
 		return 1;
 	}
 
-#if defined(CRTS_HAVE_client) || defined(CRTS_HAVE_server)
 	const struct sock_impl *socks = NULL;
+	bool client_init = false, server_init = false;
 
-	if (opts.launcher.mode & mode_online) {
-		socks = get_sock_impl(sock_impl_type_system);
-		socks->init();
-	}
-#endif
+	while (true) {
 
-#ifdef CRTS_HAVE_terragen
-	if (opts.launcher.mode & mode_terragen) {
-		terragen_main(&opts.terragen);
-		return 0;
-	}
-#endif
+		if (opts.launcher.mode & mode_online && !socks) {
+			socks = get_sock_impl(sock_impl_type_system);
+			socks->init();
+		}
 
 #ifdef CRTS_HAVE_server
-	if (opts.launcher.mode & mode_server) {
-		static struct server server = { 0 };
-		rt.server = &server;
+		if (opts.launcher.mode & mode_server) {
+			rt.server = &server;
 
-		if (!init_server(rt.server, &opts.launcher.wl, &opts.server)) {
-			return 1;
-		}
-
-		if (opts.launcher.mode & mode_online) {
-			struct sock_addr addr;
-			socks->addr_init(&addr, opts.launcher.net_addr.port);
-
-			static struct msgr_transport_rudp_ctx server_rudp_ctx = { 0 };
-			if (!msgr_transport_init_rudp(&server_rudp_ctx, &rt.server->msgr, socks, &addr)) {
+			if (!server_init) {
+				if (!init_server(rt.server, &opts.launcher.wl)) {
+					return 1;
+				}
+				server_init = true;
+			} else if (!reset_server(rt.server, &opts.launcher.wl)) {
 				return 1;
 			}
+
+			if (opts.launcher.mode & mode_online) {
+				struct sock_addr addr;
+				socks->addr_init(&addr, opts.launcher.net_addr.port);
+
+				if (!msgr_transport_init_rudp(&server_rudp_ctx, &rt.server->msgr, socks, &addr)) {
+					return 1;
+				}
+			}
 		}
-	}
 #endif
 
 #ifdef CRTS_HAVE_client
-	if (opts.launcher.mode & mode_client) {
-		static struct client client = { 0 };
-		rt.client = &client;
+		if (opts.launcher.mode & mode_client) {
+			rt.client = &client;
 
-		if (!init_client(rt.client, &opts.client)) {
-			return 1;
-		}
-
-		if (opts.launcher.mode & mode_online) {
-			static struct sock_addr server_addr = { 0 };
-			rt.server_addr = &server_addr;
-
-			socks->addr_init(rt.server_addr, opts.launcher.net_addr.port);
-			if (!socks->resolve(rt.server_addr, opts.launcher.net_addr.ip)) {
+			if (!client_init) {
+				if (!init_client(rt.client, &opts.client)) {
+					return 1;
+				}
+				client_init = true;
+			} else if (!reset_client(rt.client, &opts.client)) {
 				return 1;
 			}
 
-			struct sock_addr addr = { 0 };
-			static struct msgr_transport_rudp_ctx client_rudp_ctx = { 0 };
-			if (!msgr_transport_init_rudp(&client_rudp_ctx, rt.client->msgr, socks, &addr)) {
-				return 1;
+			if (opts.launcher.mode & mode_online) {
+				rt.server_addr = &server_addr;
+
+				socks->addr_init(rt.server_addr, opts.launcher.net_addr.port);
+				if (!socks->resolve(rt.server_addr, opts.launcher.net_addr.ip)) {
+					return 1;
+				}
+
+				struct sock_addr addr = { 0 };
+				if (!msgr_transport_init_rudp(&client_rudp_ctx, rt.client->msgr, socks, &addr)) {
+					return 1;
+				}
 			}
+
+			rt.run = &rt.client->run;
+		}
+#endif
+
+		if (!(opts.launcher.mode & mode_online)) {
+			msgr_transport_init_basic_pipe(rt.client->msgr, &rt.server->msgr);
 		}
 
-		rt.run = &rt.client->run;
+		main_loop(&rt);
 	}
-#endif
+/* #ifdef CRTS_HAVE_client */
+/* 	if (opts.launcher.mode & mode_client) { */
+/* 		deinit_client(rt.client); */
+/* 	} */
+/* #endif */
 
-#if defined(CRTS_HAVE_client) && defined(CRTS_HAVE_server)
-	if (!(opts.launcher.mode & mode_online)) {
-		msgr_transport_init_basic_pipe(rt.client->msgr, &rt.server->msgr);
-	}
-#endif
-
-	main_loop(&rt);
-
-#ifdef CRTS_HAVE_client
-	if (opts.launcher.mode & mode_client) {
-		deinit_client(rt.client);
-	}
-#endif
-
-#if defined(CRTS_HAVE_client) || defined(CRTS_HAVE_server)
-	if (socks) {
-		socks->deinit();
-	}
-#endif
+/* #if defined(CRTS_HAVE_client) || defined(CRTS_HAVE_server) */
+/* 	if (socks) { */
+/* 		socks->deinit(); */
+/* 	} */
+/* #endif */
 }
